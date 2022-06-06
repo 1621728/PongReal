ae567a5f789a75b34d779770854551d23d203f80
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-06-06 13:40:23 -0700

    ...

---
+++ Assets/Scripts/DotMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotMove : MonoBehaviour
+{
+    //floats
+    public float speed;
+    public float time;
+
+
+    //Bools
+
+    //Ints
+
+    //Strings
+
+    //Components
+    private Rigidbody2D rb2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.deltaTime;
+
+        Vector2 thrust = new Vector2(this.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude, this.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude);
+        this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * time, speed * time);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+
+        
+
+
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    //floats
+    public float speed;
+    public float time;
+    public float bounds;
+
+
+    //Bools
+
+    //Ints
+
+    //Strings
+
+    //Components
+    private Rigidbody2D rb2;
+    private Transform xt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = Time.deltaTime;
+
+        rb2 = GetComponent<Rigidbody2D>();
+
+        xt = this.gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float v = Input.GetAxis("Vertical");
+
+        Vector3 tempVect = new Vector2(0, v);
+        tempVect = tempVect.normalized * speed * time;
+
+        this.gameObject.transform.position += tempVect;
+
+    }
+}
