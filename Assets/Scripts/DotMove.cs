92c7c07e0fa2fb7284bcf05b904380f1e0ca0e08
MUKILTEO\1621728 <1621728@mukilteo.wednet.edu>
2022-06-07 12:39:35 -0700

    ...

--- Assets/Scripts/DotMove.cs
+++ Assets/Scripts/DotMove.cs
@@ -7,7 +7,7 @@ public class DotMove : MonoBehaviour
     //floats
     public float speed;
     public float time;
-
+    public float maxSpeed;
 
     //Bools
 
@@ -17,22 +17,42 @@ public class DotMove : MonoBehaviour
 
     //Components
     private Rigidbody2D rb2;
+
+    //Prefabs
+    public GameObject dust;
+
     // Start is called before the first frame update
     void Start()
     {
         time = Time.deltaTime;
 
+        rb2 = this.gameObject.GetComponent<Rigidbody2D>();
+
         Vector2 thrust = new Vector2(this.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude, this.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude);
         this.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(speed * time, speed * time);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        foreach (ContactPoint2D contact in collision.contacts)
+        {
+            
+            if (collision.gameObject)
+            {
+                Instantiate(dust, contact.point, Quaternion.identity);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
 
-        
+        if(rb2.velocity.magnitude > maxSpeed)
+        {
+            rb2.velocity = Vector2.ClampMagnitude(rb2.velocity, maxSpeed);
+        }
 
 
     }
---
+++ Assets/Scripts/killSelf.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killSelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float time;
+    void Start()
+    {
+        StartCoroutine(ExampleCoroutine());
+    }
+
+    IEnumerator ExampleCoroutine()
+    {
+
+
+
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
