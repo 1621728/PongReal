6dfe6a1a09fb2c2e3d7541114077065eacc7298c
DELL-E5470-LAPT\bambu <bam.bugsbunny@gmail.com>
2022-06-08 20:08:53 -0700

    ...

---
+++ Assets/Scripts/Seek.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seek : MonoBehaviour
+{
+    public GameObject target;
+
+    public float thrustScale;
+
+    private Rigidbody2D rb2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Seek1();
+    }
+
+    void Seek1()
+    {
+        Vector2 direction = new Vector2(target.transform.position.x - transform.position.x, target.transform.position.y - transform.position.y);
+        transform.up = direction;
+
+        //MoveForward
+        rb2.AddForce(transform.up * thrustScale * Time.deltaTime);
+    }
+}
